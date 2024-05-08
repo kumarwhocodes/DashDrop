bc1cd8d0df1cf2b81f0f045221507ea366d5c289
adityatulsyan03 <tulsyanaditya03@gmail.com>
2024-05-08 10:33:43 +0530

    Cart Screen

--- app/src/main/java/com/dashdrop/presentation/screens/CartScreen.kt
+++ app/src/main/java/com/dashdrop/presentation/screens/CartScreen.kt
@@ -1,9 +1,73 @@
 package com.dashdrop.presentation.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dashdrop.R
+import com.dashdrop.presentation.viewmodels.BottomNavOptions
+import com.dashdrop.presentation.viewmodels.SignInViewModel
+import com.dashdrop.ui.components.BottomNavBar
+import com.dashdrop.ui.components.CartButton
+import com.dashdrop.ui.components.ItemButton
+import com.dashdrop.ui.components.ScaffoldTop
 
 @Composable
-fun CartScreen() {
+fun CartScreen(signInViewModel: SignInViewModel = viewModel()) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            ScaffoldTop(toolbarTitle = "Cart",
+                logOutButtonClicked = {
+                    signInViewModel.logout()
+                },
+                navigationIconClicked = {
+                    /*TODO: Back Button Logic*/
+                })
+        },
+        bottomBar = {
+            BottomNavBar(bottomMenu = BottomNavOptions.menuItems)
+        }
+    ) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(10.dp)
+        ) {
+            LazyColumn() {
+                items(5){
+                    Row(){
+                        CartButton(
+                            value = "Veggies",
+                            image = painterResource(id = R.drawable.veggiess),
+                            price = "150",
+                            startCount = 2.0
+                        )
+                        CartButton(
+                            value = "Veggies",
+                            image = painterResource(id = R.drawable.veggiess),
+                            price = "150",
+                            startCount = 3.0
+                        )
+                    }
+                }
+            }
+        }
 
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    CartScreen()
 }
