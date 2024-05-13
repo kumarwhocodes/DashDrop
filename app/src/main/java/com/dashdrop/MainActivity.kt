3e75139c5da609d7b39b7e6259b1280f8f916e3f
Kumar Sambhav <sambhav26k@gmail.com>
2024-05-13 22:12:00 +0530

    Splash Screen Done

--- app/src/main/java/com/dashdrop/MainActivity.kt
+++ app/src/main/java/com/dashdrop/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.dashdrop
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.dashdrop.navigation.Navigation
 import com.dashdrop.ui.theme.DashDropTheme
@@ -10,7 +15,11 @@ import com.dashdrop.ui.theme.DashDropTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                false
+            }
+        }
         setContent {
             DashDropTheme {
                 val navController = rememberNavController()
