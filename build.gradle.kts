63258bb84f709216ef6a1abc658adc4ad92a7754
Kumar Sambhav <sambhav26k@gmail.com>
2024-06-12 16:54:23 +0530

    fix - ImageSlider.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     // Add the dependency for the Hilt Gradle plugin
     id("com.google.dagger.hilt.android") version "2.48" apply false
 
@@ -12,9 +12,8 @@ buildscript {
         google()
     }
     dependencies {
-        val nav_version = "2.7.7"
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.49")
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+        classpath(libs.hilt.android.gradle.plugin)
 
     }
 }
